e812b1fe55c12d1da7e725cceeb7a3eca319ca62
Felipe-builder <felipesvascon@gmail.com>
2022-05-03 22:33:51 -0300

    protegendo as senhas no banco de dados

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use(express.json())
+
 app.use(
   bodyParser.urlencoded({
     extended: true
