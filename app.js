a5050fb6649759859c3f85e20c6ec03ba95da0e8
Felipe-builder <felipesvascon@gmail.com>
2022-05-06 21:32:15 -0300

    Criando um sistema de login escalavel

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const { estrategiasAutenticacao } = require('./src/usuarios');
+
 app.use(express.json())
 
 app.use(
