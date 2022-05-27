f1e9ea846fb491e6e75e9c0f1bc99bdbe80ae8f2
Felipe-builder <felipesvascon@gmail.com>
2022-05-26 23:15:28 -0300

    verificacao de email completa

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -76,12 +76,24 @@ module.exports = {
 
     async verificacaoEmail(req, res, next) {
         try {
-            const { id } = req.params;
+            const { token } = req.params;
+            const id = await tokens.verificacaoEmail.verifica(token);
             const usuario = await Usuario.buscaPorId(id);
             req.user = usuario;
             next(); 
         } catch(erro) {
-            return res.status(500).json({ erro: erro.message });
+            if (erro.name === 'JsonWebTokenError') {
+                return res.status(401).json({ erro: erro.message});
+            }
+
+            if (erro.name === 'TokenExpiredError') {
+                return res.status(401).json({ 
+                    erro: erro.message,
+                    expiradoEm: erro.expiredAt
+                })
+            }
+
+            return res.status(500).json({ erro: erro.message }); 
         }
     }
 }
