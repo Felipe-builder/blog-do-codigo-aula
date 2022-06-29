2cc869c9141a6c9a1ac8492cf8ea8fed290d90d5
Felipe-builder <felipesvascon@gmail.com>
2022-06-28 23:14:20 -0300

    Filtrando atributos

--- src/posts/posts-controlador.js
+++ src/posts/posts-controlador.js
@@ -22,7 +22,7 @@ module.exports = {
   async lista(req, res) {
     try {
       let posts = await Post.listarTodos();
-      const conversor = new ConversorPost('json')
+      const conversor = new ConversorPost('json', req.acesso.todos.permitido ? req.acesso.todos.atributos : req.acesso.apenasSeu.atributos)
       if (!req.estaAutenticado) {
         posts = posts.map(post => {
           post.conteudo = post.conteudo.substr(0, 10) + '... Você precisa assinar o blog para ler o restante do post'
